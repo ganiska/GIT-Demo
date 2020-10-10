d917f6efb881d5df78d1f2f33db0b714164746e7
ganiska <ganiska96@gmail.com>
2020-10-10 21:25:50 +0530

    number 4 added

--- AddTwoNo.py
+++ AddTwoNo.py
@@ -3,9 +3,10 @@
 num1 = 1.5
 num2 = 6.3
 num3 = 4.5
+num4 = 7.7
 
 # Add two numbers
-sum = num1 + num2 + num3
+sum = num1 + num2 + num3 = num4
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
