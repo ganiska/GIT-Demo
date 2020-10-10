e667b3675e0f0035fc90d26f808a862bb6cbaed1
udithindrakantha <udith.indrakantha@gmail.com>
2020-10-10 21:57:23 +0530

    new file added

---
+++ add5numbers.py
@@ -0,0 +1,16 @@
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+num3 = 4.5
+num4 = 7.7
+numb5 = 54.5
+
+# Add two numbers
+sum = num1 + num2 + num3 + num4+ numb5
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+
+#udith added this first comment
